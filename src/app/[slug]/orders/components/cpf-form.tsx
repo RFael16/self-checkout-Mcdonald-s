f248ad96391b7085e9e7f18b5e37e61c39d65ea2
Rafael Dev <rafaeldesenvolvedor49@gmail.com>
2026-02-17 18:29:12 -0300

    feat: add orders oage

---
+++ src/app/[slug]/orders/components/cpf-form.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
+import { usePathname, useRouter } from "next/navigation";
+import { Form, useForm } from "react-hook-form";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer"
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { isValidCpf, removeCpfPunctuation } from "../../menu/helpers/cpf";
+
+const formSchema = z.object({
+    cpf: z.string().trim().min(1, {message: "O CPF é obrigatório"}).refine((value) => isValidCpf(value), {message: "CPF inválido"}),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+const CpfForm = () => {
+    const form = useForm<FormSchema>({resolver: zodResolver(formSchema)
+    });
+
+    const router = useRouter();
+    const pathname = usePathname()
+    const onSubmit = (data: FormSchema) => {
+        router.push(`${pathname}?cpf=${removeCpfPunctuation(data.cpf)}`); // Redireciona para a página de pedidos, passando o CPF como query param
+    };
+
+    const handleCancel = () => {
+        router.back();
+    }
+    
+    return ( 
+        <Drawer open>
+            <DrawerContent>
+                <DrawerHeader>
+                <DrawerTitle>Visualizar Pedidos</DrawerTitle>
+                <DrawerDescription>Insira seu CPF para visualizar seus pedidos.</DrawerDescription>
+                </DrawerHeader>
+                    <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-8">
+                                  <FormField
+                                    control={form.control}
+                                    name="cpf"
+                                    render={({ field }) => (
+                                      <FormItem  className="px-4">
+                                        <FormLabel>Seu CPF</FormLabel>
+                                        <FormControl>
+                                          <Input placeholder="Digite seu CPF..." {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                      </FormItem>
+                                    )}
+                                  />
+                                  <DrawerFooter>
+                                    <Button variant="destructive" className="w-full rounded-full">Confirmar</Button>
+                                    <DrawerClose asChild>
+                                        <Button variant="outline" className="w-full rounded-full" onClick={handleCancel}>Cancelar</Button>
+                                    </DrawerClose>
+                                 </DrawerFooter>
+                    </form>
+                    </Form>
+            </DrawerContent>
+        </Drawer>
+     );
+}
+ 
+export default CpfForm;
